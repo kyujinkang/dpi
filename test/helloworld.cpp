35325b5b0674cec3f6086c112b45b68b1b8593e6
kyujin.kang <kyujin.kang@samsung.com>
2015-08-07 16:47:30 +0900

    add 10 loop message

--- test/helloworld.cpp
+++ test/helloworld.cpp
@@ -6,6 +6,10 @@ int main(int argc, char *argv) {
   cout << "Hello world!" << endl;
 
   cout << "Hello world2!" << endl;
+
+  for (int i = 0; i < 10; i++) {
+	  cout << "Hello world[" << i << "]!!" << endl;
+  }
   
   return 0;
 }
