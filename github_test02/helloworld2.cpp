7e089e4bc5268eb39c67d7833c1b05336ff96e62
JaeSeok <js77.jung@samsung.com>
2015-08-10 15:16:39 +0900

    function추가

--- github_test02/helloworld2.cpp
+++ github_test02/helloworld2.cpp
@@ -2,13 +2,18 @@
 
 using namespace std;
 
+void repeat(int n)
+{
+	for (int i = 0; i < n; i++)
+		cout << "Hello world!" << endl;
+}
+
 int main(int argc, char *argv) {
 	cout << "Hello world!" << endl;
 
 	cout << "Hello world 2 !" << endl;
 
-	for (int i = 0; i < 10; i++)
-		cout << "Hello world!" << endl;
+	repeat(10);
 
 	return 0;
 }
